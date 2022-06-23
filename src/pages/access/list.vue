a7144a02135d7390a01ea54220e7f115747e83bb
pikaka-web <1289265046@qq.com>
2022-06-23 16:04:04 +0800

    树形控件自定义节点渲染

--- src/pages/access/list.vue
+++ src/pages/access/list.vue
@@ -1,13 +1,59 @@
 <template>
-  <el-card shadow="never">
-    <ListHerder @refresh="getData" />
+  <el-card shadow="never" class="border-0">
+    <ListHeader @create="handleCreate" @refresh="getData" />
     <el-tree
       :data="tableData"
       :props="{ label: 'name', children: 'child' }"
       v-loading="loading"
       node-key="id"
       :default-expanded-keys="defaultExpandedKeys"
-    ></el-tree>
+    >
+      <template #default="{ data }">
+        <div class="custom-tree-node">
+          <el-tag size="small" :type="data.menu ? '' : 'info'">{{
+            data.menu ? "菜单" : "权限"
+          }}</el-tag>
+          <el-icon v-if="data.icon" :size="16" class="ml-2">
+            <component :is="data.icon" />
+          </el-icon>
+          <span>{{ data.name }}</span>
+
+          <div class="ml-auto">
+            <el-switch
+              :modelValue="data.status"
+              :active-value="1"
+              :inactive-value="0"
+              @change="handleStatusChange($event, data)"
+            />
+            <el-button
+              text
+              type="primary"
+              size="small"
+              @click.stop="handleEdit(data)"
+              >修改</el-button
+            >
+            <el-button
+              text
+              type="success"
+              size="small"
+              @click.stop="addChild(data.id)"
+              >增加</el-button
+            >
+
+            <el-popconfirm
+              title="是否要删除该记录？"
+              confirmButtonText="确认"
+              cancelButtonText="取消"
+              @confirm="handleDelete(data.id)"
+            >
+              <template #reference>
+                <el-button text type="danger" size="small">删除</el-button>
+              </template>
+            </el-popconfirm>
+          </div>
+        </div>
+      </template>
+    </el-tree>
   </el-card>
 </template>
 
@@ -28,3 +74,16 @@ const { loading, tableData, getData } = useInitTable({
   },
 });
 </script>
+
+<style scoped>
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding-right: 8px;
+}
+.el-tree-node__content {
+  padding: 20px 0;
+}
+</style>
