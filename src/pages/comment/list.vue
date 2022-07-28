26ecd3b87a021e55472c1ea234e36283a306529f
pikaka-web <1289265046@qq.com>
2022-07-28 13:13:50 +0800

    快速初始化商品评论页面

--- src/pages/comment/list.vue
+++ src/pages/comment/list.vue
@@ -1,9 +1,129 @@
 <template>
-  <div>评论列表</div>
+  <el-card shadow="never" class="border-0">
+    <!-- 搜索 -->
+    <Search :model="searchForm" @search="getData" @reset="resetSearchForm">
+      <SearchItem label="关键词">
+        <el-input
+          v-model="searchForm.title"
+          placeholder="商品标题"
+          clearable
+        ></el-input>
+      </SearchItem>
+    </Search>
+
+    <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
+      <el-table-column label="管理员" width="200">
+        <template #default="{ row }">
+          <div class="flex items-center">
+            <el-avatar :size="40" :src="row.avatar">
+              <img
+                src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"
+              />
+            </el-avatar>
+            <div class="ml-3">
+              <h6>{{ row.username }}</h6>
+              <small>ID:{{ row.id }}</small>
+            </div>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="所属角色" align="center">
+        <template #default="{ row }">
+          {{ row.role?.name || "-" }}
+        </template>
+      </el-table-column>
+      <el-table-column label="状态" width="120">
+        <template #default="{ row }">
+          <el-switch
+            :modelValue="row.status"
+            :active-value="1"
+            :inactive-value="0"
+            :loading="row.statusLoading"
+            :disabled="row.super == 1"
+            @change="handleStatusChange($event, row)"
+          >
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="180" align="center">
+        <template #default="scope">
+          <small v-if="scope.row.super == 1" class="text-sm text-gray-500"
+            >暂无操作</small
+          >
+          <div v-else>
+            <el-button
+              type="primary"
+              size="small"
+              text
+              @click="handleEdit(scope.row)"
+              >修改</el-button
+            >
+
+            <el-popconfirm
+              title="是否要删除该管理员？"
+              confirmButtonText="确认"
+              cancelButtonText="取消"
+              @confirm="handleDelete(scope.row.id)"
+            >
+              <template #reference>
+                <el-button text type="primary" size="small">删除</el-button>
+              </template>
+            </el-popconfirm>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div class="flex items-center justify-center mt-5">
+      <el-pagination
+        background
+        layout="prev, pager ,next"
+        :total="total"
+        :current-page="currentPage"
+        :page-size="limit"
+        @current-change="getData"
+      />
+    </div>
+  </el-card>
 </template>
-
 <script setup>
-</script>
+import { ref } from "vue";
+import Search from "~/components/Search.vue";
+import SearchItem from "~/components/SearchItem.vue";
 
-<style scoped>
-</style>
+import {
+  getGoodsCommentList,
+  updateGoodsCommentStatus,
+} from "~/api/goods_comment";
+
+import { useInitTable } from "~/composables/useCommon.js";
+
+const roles = ref([]);
+
+const {
+  searchForm,
+  resetSearchForm,
+  tableData,
+  loading,
+  currentPage,
+  total,
+  limit,
+  getData,
+  handleDelete,
+  handleStatusChange,
+} = useInitTable({
+  searchForm: {
+    title: "",
+  },
+  getList: getGoodsCommentList,
+  onGetListSuccess: (res) => {
+    tableData.value = res.list.map((o) => {
+      o.statusLoading = false;
+      return o;
+    });
+    total.value = res.totalCount;
+    roles.value = res.roles;
+  },
+  updateStatus: updateGoodsCommentStatus,
+});
+</script>
